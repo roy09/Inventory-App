5fb85ee12a95cff1eb7e03d5d6d139feb709265f
raihan18 <raihan.nayeem@gmail.com>
2014-08-08 20:00:59 +0600

    + customer dashboard, userinfo

---
+++ src/com/cse470/osia/UserInfoActivity.java
@@ -0,0 +1,22 @@
+package com.cse470.osia;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class UserInfoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_info);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.user_info, menu);
+		return true;
+	}
+
+}
